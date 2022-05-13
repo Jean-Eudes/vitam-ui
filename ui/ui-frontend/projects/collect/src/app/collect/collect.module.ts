546ea8d4f0cf91a32da9d8fb999d133918a128ac
Abdelmoez GUETAT <abdelmoez.guetat@smile.fr>
2022-05-13 11:30:26 +0200

    add collect service

---
+++ ui/ui-frontend/projects/collect/src/app/collect/collect.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CollectRoutingModule } from './collect-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    CollectRoutingModule
+  ]
+})
+export class CollectModule { }
